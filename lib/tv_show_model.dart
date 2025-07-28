f130cca1ad9652624ae95fdc579db95f85ee18ae
renata <renata.coelho@icomp.ufam.edu.br>
2025-07-28 00:11:51 -0400

    Versão inicial do app de séries

---
+++ lib/tv_show_model.dart
@@ -0,0 +1,98 @@
+import 'dart:math';
+
+import 'package:app3_series_api/tv_show_service.dart';
+import 'package:flutter/material.dart';
+
+class TvShow {
+  int id;
+  String imageUrl;
+  String name;
+  String webChannel;
+  double rating;
+  String summary;
+
+  TvShow({
+    required this.id,
+    required this.imageUrl,
+    required this.name,
+    required this.webChannel,
+    required this.rating,
+    required this.summary,
+  });
+
+  factory TvShow.fromJson(Map<String, dynamic> json) {
+    return TvShow(
+      id: json['id'],
+      imageUrl:
+          json['image']?['medium'] ??
+          '', // ? indica a possibilidade de não haver imagem, valor nulo
+      name: json['name'],
+      webChannel: json['webChannel']?['name'] ?? 'N/A',
+      rating: json['rating']?['average']?.toDouble ?? 0.0,
+      summary: json['summary'] ?? 'Sem resumo disponível.',
+    );
+  }
+}
+
+class TvShowModel extends ChangeNotifier {
+  final TvShowService _tvShowService = TvShowService();
+
+  final List<TvShow> _tvShows = [];
+  List<TvShow> get tvShows => _tvShows;
+
+  Future<List<TvShow>> searchTvShows(String query) async {
+    try {
+      return await _tvShowService.fetchTvShow(query);
+    } catch (e) {
+      throw Exception('Falha em buscar séries: ${e.toString()}');
+    }
+  }
+
+  void addTvShow(TvShow tvShow, BuildContext context) {
+    tvShows.add(tvShow);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(    
+        content: Text(
+          'Série adicionada com sucesso!',
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+      ),
+    );
+    notifyListeners();
+  }
+
+  void removeTvShow(TvShow tvShow, BuildContext context) {
+    final index = tvShows.indexWhere(
+      (show) => show.name.toLowerCase() == tvShow.name.toLowerCase(),
+    );
+    tvShows.removeAt(index);
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${tvShow.name} excluída!'),
+        duration: Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'DESFAZER',
+          onPressed: () {
+            tvShows.insert(index, tvShow);
+            notifyListeners();
+          },
+        ),
+      ),
+    );
+    notifyListeners();
+  }
+
+  void editTvShow(TvShow oldTvShow, TvShow newTvShow, BuildContext context) {
+    final index = tvShows.indexOf(oldTvShow);
+    tvShows[index] = newTvShow;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Série ${index + 1} atualizada!'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+    notifyListeners();
+  }
+}
---
+++ lib/tv_show_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:app3_series_api/tv_show_model.dart';
+import 'package:http/http.dart' as http;
+
+class TvShowService { //me comunicar com a api
+  Future<List<TvShow>> fetchTvShow(String query) async {
+    final response = await http.get(
+      Uri.parse('https://www.tvmaze.com/search/shows?q=$query'),
+    );
+
+    if (response.statusCode == 200) {
+      final List<TvShow> tvShows = [];
+      json.decode(response.body).forEach((item) {
+        tvShows.add(TvShow.fromJson(item['show']));
+      });
+      return tvShows;
+    } else {
+      throw Exception('Falha ao carregar séries!');
+    }
+  }
+}
